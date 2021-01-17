6020ba1cc7e166d0d58afc35b068386330cc67df
evgeniyBolnov <evgeniy.bolnov@gmail.com>
2021-01-17 20:56:56 +0300

    Convert tabs to spaces

--- rtl/draw_cross.sv
+++ rtl/draw_cross.sv
@@ -1,95 +1,95 @@
 module draw_cross #(
-	parameter WIDTH  = 800,
-	parameter HEIGHT = 600
+  parameter WIDTH  = 800,
+  parameter HEIGHT = 600
 ) (
-	input                             clk      ,
-	input                             reset    ,
-	input        [ $clog2(WIDTH)-1:0] x        ,
-	input        [$clog2(HEIGHT)-1:0] y        ,
-	input                             frame,
-	output logic [              23:0] data
+  input                             clk      ,
+  input                             reset    ,
+  input        [ $clog2(WIDTH)-1:0] x        ,
+  input        [$clog2(HEIGHT)-1:0] y        ,
+  input                             frame,
+  output logic [              23:0] data
 );
 
-	localparam CROSS_SIZE = 100;
+  localparam CROSS_SIZE = 100;
 
-	logic [ $clog2(WIDTH):0] cross_X;
-	logic [$clog2(HEIGHT):0] cross_Y;
-	logic dir_x;
-	logic dir_y;
+  logic [ $clog2(WIDTH):0] cross_X;
+  logic [$clog2(HEIGHT):0] cross_Y;
+  logic dir_x;
+  logic dir_y;
 
-	typedef enum logic [23:0]{
-		BLACK = 24'h012345,
-		RED   = 24'hFF0000,
-		GREEN = 24'h00FF00,
-		BLUE  = 24'h0000FF
-	} color_t;
+  typedef enum logic [23:0]{
+    BLACK = 24'h012345,
+    RED   = 24'hFF0000,
+    GREEN = 24'h00FF00,
+    BLUE  = 24'h0000FF
+  } color_t;
 
-	color_t color;
+  color_t color;
 
-	always_ff @(posedge clk or posedge reset)
-		begin
-			if (reset)
-				begin
-					cross_X <= '0;
-					cross_Y <= '0;
-					dir_x   <= '0;
-					dir_y   <= '0;
-					color   <= color.first();
-				end
-			else
-				begin
-					if (frame)
-						begin
-							if (dir_x)
-								begin
-									if (cross_X < ( WIDTH - CROSS_SIZE ) )
-										cross_X <= cross_X + 1'b1;
-									else
-										begin
-											dir_x <= ~dir_x;
-											color <= color.next();
-										end
-								end
-							else
-								begin
-									if (cross_X > 0)
-										cross_X <= cross_X - 1'b1;
-									else
-										begin
-											dir_x <= ~dir_x;
-											color <= color.next();
-										end
-								end
-							if (dir_y)
-								begin
-									if (cross_Y < ( HEIGHT - CROSS_SIZE ) )
-										cross_Y <= cross_Y + 1'b1;
-									else
-										begin
-											dir_y <= ~dir_y;
-											color <= color.next();
-										end
-								end
-							else
-								begin
-									if (cross_Y > 0)
-										cross_Y <= cross_Y - 1'b1;
-									else
-										begin
-											dir_y <= ~dir_y;
-											color <= color.next();
-										end
-								end
-						end
-				end
-		end
+  always_ff @(posedge clk or posedge reset)
+    begin
+      if (reset)
+        begin
+          cross_X <= '0;
+          cross_Y <= '0;
+          dir_x   <= '0;
+          dir_y   <= '0;
+          color   <= color.first();
+        end
+      else
+        begin
+          if (frame)
+            begin
+              if (dir_x)
+                begin
+                  if (cross_X < ( WIDTH - CROSS_SIZE ) )
+                    cross_X <= cross_X + 1'b1;
+                  else
+                    begin
+                      dir_x <= ~dir_x;
+                      color <= color.next();
+                    end
+                end
+              else
+                begin
+                  if (cross_X > 0)
+                    cross_X <= cross_X - 1'b1;
+                  else
+                    begin
+                      dir_x <= ~dir_x;
+                      color <= color.next();
+                    end
+                end
+              if (dir_y)
+                begin
+                  if (cross_Y < ( HEIGHT - CROSS_SIZE ) )
+                    cross_Y <= cross_Y + 1'b1;
+                  else
+                    begin
+                      dir_y <= ~dir_y;
+                      color <= color.next();
+                    end
+                end
+              else
+                begin
+                  if (cross_Y > 0)
+                    cross_Y <= cross_Y - 1'b1;
+                  else
+                    begin
+                      dir_y <= ~dir_y;
+                      color <= color.next();
+                    end
+                end
+            end
+        end
+    end
 
-	always_comb
-		begin
-			if (x < (cross_X + CROSS_SIZE) && x >= cross_X && y < (cross_Y + CROSS_SIZE) && y >= cross_Y )
-				data = color;
-			else
-				data = '0;
-		end
+  always_comb
+    begin
+      if (x < (cross_X + CROSS_SIZE) && x >= cross_X && y < (cross_Y + CROSS_SIZE) && y >= cross_Y )
+        data = color;
+      else
+        data = '0;
+    end
 
 endmodule
--- rtl/frame_gen.sv
+++ rtl/frame_gen.sv
@@ -1,91 +1,91 @@
 module frame_gen #(
-	parameter WIDTH   = 800,
-	parameter HEIGHT  = 600,
-	parameter H_FRONT = 40 ,
-	parameter H_SYNC  = 128,
-	parameter H_BACK  = 88 ,
-	parameter V_FRONT = 1  ,
-	parameter V_SYNC  = 4  ,
-	parameter V_BACK  = 23
+  parameter WIDTH   = 800,
+  parameter HEIGHT  = 600,
+  parameter H_FRONT = 40 ,
+  parameter H_SYNC  = 128,
+  parameter H_BACK  = 88 ,
+  parameter V_FRONT = 1  ,
+  parameter V_SYNC  = 4  ,
+  parameter V_BACK  = 23
 ) (
-	input                             clk   ,
-	input                             reset ,
-	output logic                      h_sync,
-	output logic                      v_sync,
-	output logic                      de    ,
-	output logic [              23:0] data  ,
-	output       [ $clog2(WIDTH)-1:0] x     ,
-	output       [$clog2(HEIGHT)-1:0] y,
-	output                            frame
+  input                             clk   ,
+  input                             reset ,
+  output logic                      h_sync,
+  output logic                      v_sync,
+  output logic                      de    ,
+  output logic [              23:0] data  ,
+  output       [ $clog2(WIDTH)-1:0] x     ,
+  output       [$clog2(HEIGHT)-1:0] y,
+  output                            frame
 );
 
-	localparam full_x  = WIDTH+H_FRONT+H_SYNC+H_BACK ;
-	localparam full_y  = HEIGHT+V_FRONT+V_SYNC+V_BACK;
-	localparam blank_x = H_FRONT+H_SYNC+H_BACK       ;
-	localparam blank_y = V_FRONT+V_SYNC+V_BACK       ;
+  localparam full_x  = WIDTH+H_FRONT+H_SYNC+H_BACK ;
+  localparam full_y  = HEIGHT+V_FRONT+V_SYNC+V_BACK;
+  localparam blank_x = H_FRONT+H_SYNC+H_BACK       ;
+  localparam blank_y = V_FRONT+V_SYNC+V_BACK       ;
 
-	logic [$clog2(full_x)-1:0] pix_cnt ;
-	logic [$clog2(full_y)-1:0] line_cnt;
+  logic [$clog2(full_x)-1:0] pix_cnt ;
+  logic [$clog2(full_y)-1:0] line_cnt;
 
-	assign x = pix_cnt - blank_x;
-	assign y = line_cnt - blank_y;
+  assign x = pix_cnt - blank_x;
+  assign y = line_cnt - blank_y;
 
 `ifdef MODELSIM
-	event end_frame;
+  event end_frame;
 `endif
 
-	always_ff @(posedge clk or posedge reset)
-		if (reset)
-			begin
-				pix_cnt  <= '0;
-				line_cnt <= '0;
-				frame    <= '0;
-			end
-		else
-			begin
-				if (pix_cnt < full_x - 1'b1)
-					begin
-						frame   <= '0;
-						pix_cnt <= pix_cnt + 1'b1;
-					end
-				else
-					begin
-						pix_cnt <= '0;
-						if (line_cnt < full_y - 1'b1)
-							begin
-								line_cnt <= line_cnt + 1'b1;
-								frame    <= '0;
-							end
-						else
-							begin
-								`ifdef MODELSIM
-									-> end_frame;
-								`endif
-								frame    <= '1;
-								line_cnt <= '0;
-							end
-					end
-			end
+  always_ff @(posedge clk or posedge reset)
+    if (reset)
+      begin
+        pix_cnt  <= '0;
+        line_cnt <= '0;
+        frame    <= '0;
+      end
+    else
+      begin
+        if (pix_cnt < full_x - 1'b1)
+          begin
+            frame   <= '0;
+            pix_cnt <= pix_cnt + 1'b1;
+          end
+        else
+          begin
+            pix_cnt <= '0;
+            if (line_cnt < full_y - 1'b1)
+              begin
+                line_cnt <= line_cnt + 1'b1;
+                frame    <= '0;
+              end
+            else
+              begin
+                `ifdef MODELSIM
+                  -> end_frame;
+                `endif
+                frame    <= '1;
+                line_cnt <= '0;
+              end
+          end
+      end
 
-	always_ff @(posedge clk or posedge reset)
-		begin
-			if (reset)
-				begin
-					h_sync <= '0;
-					v_sync <= '0;
-					de     <= '0;
-					data   <= '0;
-				end
-			else
-				begin
-					h_sync <= (pix_cnt  < H_BACK || pix_cnt  > (H_SYNC + H_BACK - 1'b1)) ? 1'b1 : 1'b0;
-					v_sync <= (line_cnt < V_BACK || line_cnt > (V_SYNC + V_BACK - 1'b1)) ? 1'b1 : 1'b0;
-					de     <= (pix_cnt  > (blank_x - 1'b1) && line_cnt > (blank_y - 1'b1) ) ? 1'b1 : 1'b0;
-					if (pix_cnt > (blank_x - 1'b1) && line_cnt > (blank_y - 1'b1) )
-						data <= x;
-					else
-						data <= '0;
-				end
-		end
+  always_ff @(posedge clk or posedge reset)
+    begin
+      if (reset)
+        begin
+          h_sync <= '0;
+          v_sync <= '0;
+          de     <= '0;
+          data   <= '0;
+        end
+      else
+        begin
+          h_sync <= (pix_cnt  < H_BACK || pix_cnt  > (H_SYNC + H_BACK - 1'b1)) ? 1'b1 : 1'b0;
+          v_sync <= (line_cnt < V_BACK || line_cnt > (V_SYNC + V_BACK - 1'b1)) ? 1'b1 : 1'b0;
+          de     <= (pix_cnt  > (blank_x - 1'b1) && line_cnt > (blank_y - 1'b1) ) ? 1'b1 : 1'b0;
+          if (pix_cnt > (blank_x - 1'b1) && line_cnt > (blank_y - 1'b1) )
+            data <= x;
+          else
+            data <= '0;
+        end
+    end
 
 endmodule
